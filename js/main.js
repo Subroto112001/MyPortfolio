d6878dcfe910064b3316d9df710d5fe5b653575d
Subroto112001 <subrotokumarbarman@gmail.com>
2025-03-05 11:50:27 +0600

    Some File added

---
+++ js/main.js
@@ -0,0 +1,5 @@
+/*=========Sticky Navbar==============*/
+window.onscroll = () => {
+    let header = document.querySelector(".header");
+    header.classList.toggle("sticky", window.scrollY > 100);
+}
